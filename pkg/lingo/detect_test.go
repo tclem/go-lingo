39ffa9daf28cba90c573c61dbfbc61cc9276240f
Timothy Clem <timothy.clem@gmail.com>
2019-07-30 08:57:43 -0700

    Test out a couple more things

--- pkg/lingo/detect_test.go
+++ pkg/lingo/detect_test.go
@@ -15,3 +15,14 @@ func TestDetectByFileName(t *testing.T) {
 	lang := LanguageForPath("Rakefile")
 	assert.Equal(t, "Ruby", lang.Name)
 }
+
+func TestUnableToDetect(t *testing.T) {
+	lang := LanguageForPath("noideawhatthisis")
+	assert.Nil(t, lang)
+}
+
+func TestParsedLanguagesYml(t *testing.T) {
+	assert.Equal(t, 519, len(Languages))
+	assert.Equal(t, 1117, len(LanguagesByExtension))
+	assert.Equal(t, 235, len(LanguagesByFileName))
+}
